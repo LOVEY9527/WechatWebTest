8b774d1b50a0471ddd54e3d2dd3c7c62f319b625
li_yong <371191322@qq.com>
2016-09-29 17:45:15 +0800

    修改人：李勇 bug单号： 修改内容：添加微信小程序工程

---
+++ pages/home/home.js
@@ -0,0 +1,22 @@
+//index.js
+Page({
+  data:{
+    text: "这是一个首页",
+    array: [{"key": "789"}, {"key": "456"}]
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
